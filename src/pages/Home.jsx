afa1a81136e9d6907d16b14127f652238667cecb
moh-baraa <113168104+moh-baraa@users.noreply.github.com>
2025-05-31 06:36:53 +0300

    Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,26 +6,34 @@ import WorkoutForm from "../components/WorkoutForm"
 const Home = () => {
     const [workouts, setWorkouts] = useState(null)
     const [show, setShow] = useState(false)// to initiate the use effect
+
+    const users = JSON.parse(localStorage.getItem('user'))
+
     useEffect(() => {
         const fetchWorkouts = async () => {
             try {
-                let response = await ax.get(import.meta.env.VITE_URI + "/api/workouts");
+                let response = await ax.get(import.meta.env.VITE_URI + "/api/workouts", {
+                    headers: { 'Authorization': `Bearer ${users.token}` },
+                });
                 setWorkouts(response.data)
-            } catch (err) {
-                console.log(`ERROR! ${err}`);
+            }
+            catch (err) {
+                console.log(`ERROR! ${err.response.data.error}`);
             }
 
         }
-fetchWorkouts()
-    },[show])
-    return(
+        if (users) {
+            fetchWorkouts()
+        }
+        }, [show])
+    return (
         <div className="home">
             <div className="workouts">
                 {workouts && workouts.map(workout => ( //will not go to the function unless there workouts exist
-                    <WorkoutDetails workout={workout} setShow={setShow} show={show} key={workout._id}/>
+                    <WorkoutDetails workout={workout} setShow={setShow} show={show} key={workout._id} />
                 ))}
             </div>
-            <WorkoutForm setShow={setShow} show={show}/>
+            <WorkoutForm setShow={setShow} show={show} />
         </div>
     )
 }
