129a17b3a7b81fe99f516616e966a68835acf773
moh-baraa <113168104+moh-baraa@users.noreply.github.com>
2025-05-31 05:22:59 +0300

    Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,1 +1,52 @@
+import { useState } from "react"
+import ax from 'axios'
+import { useNavigate } from "react-router-dom";
 
+const Login = () => {
+
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [error, setError] = useState(null)
+    const [isLoading, setIsLoading] = useState(null)
+
+    const navigate = useNavigate()
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        
+        try {
+            setIsLoading(true) setError(null)
+            const response = await ax.post(import.meta.env.VITE_URI + '/api/user/login', { email, password }
+    localStorage.setItem('user', JSON.stringify(response.data))
+    setIsLoading(false)
+    navigate('/')
+    window.location.reload();
+        } catch (error) {
+        }
+        setIsLoading(false)
+        setError(error.response.data.error)
+    }catch () {
+
+    }
+
+    return (
+        <form className="login" onSubmit={handleSubmit}> <h3>Log In</h3>
+            <label>Email address:</label>
+            <input
+            />
+            type="email"
+            I
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+            <label>Password:</label>
+    
+    /><input
+                type="password"
+                onChange={(e) => setPassword(e.target.value)}
+                value={password} />
+            <button disabled={isLoading}>Log in</button>
+            {error && <div className="error">{error}</div>}
+        </form>
+    )
+}
+export default Login
