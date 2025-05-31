1a5472ae27a491486beb712be4052317bcbeb70c
moh-baraa <113168104+moh-baraa@users.noreply.github.com>
2025-05-31 06:45:46 +0300

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,30 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Navbar from './components/Navbar'
+import { BrowserRouter, Routes, Route, Navigate} from 'react-router-dom'
+
 import Home from './pages/Home'
+import Login from './pages/Login'
+import Signup from './pages/Signup'
+import Navbar from './components/Navbar'
+
 function App() {
+  const users = JSON.parse(localStorage.getItem('user'))
   return (
     <div className="App">
       <BrowserRouter>
         <Navbar />
         <div className="pages">
           <Routes>
-            <Route path="/" element={<Home />}
+            <Route path="/"
+            element={users ? <Home />: <Navigate to ="/login"/>}
             />
+
+            <Route path="/login"
+            element={!users ? <Login />: <Navigate to ="/"/>}
+            />
+
+            <Route path="/signup"
+            element={!users ? <Signup />: <Navigate to ="/"/>}
+            />
+
           </Routes>
         </div>
       </BrowserRouter>
